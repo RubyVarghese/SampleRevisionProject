62a4437ecbba403e884b4e69eae37ee9b923ebdc
rubyv <rubyv@LENOVO>
2023-04-12 07:50:04 +0530

    First commit

---
+++ SampleRevisionProject/src/main/java/sampleRevision/Student.java
@@ -0,0 +1,19 @@
+package sampleRevision;
+
+public class Student extends person {
+	int id;
+	public void print(int id)
+	{
+		this.id=id;
+		System.out.println("id is "+id);
+	
+	}
+
+	public static void main(String[] args) {
+		Student ob=new Student();
+		ob.studname("Ruby");
+		ob.print(12);
+
+	}
+
+}
---
+++ SampleRevisionProject/src/main/java/sampleRevision/person.java
@@ -0,0 +1,13 @@
+package sampleRevision;
+
+public class person {
+	String name ;
+	
+	void studname(String name)
+	{
+		this.name=name;
+		System.out.println("Name " +name);
+	}
+	
+
+}
